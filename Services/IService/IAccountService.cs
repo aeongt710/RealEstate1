cdf86cf9f70b1b498be4d0959b6e9739eb8f830f
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-06-23 01:38:42 +0500

    account.added

---
+++ Services/IService/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RealEstate1.Models;
+using RealEstate1.Models.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealEstate1.Services.IService
+{
+    public interface IAccountService
+    {
+        public Task<string> RegisterNewUser(RegisterVM registerVM);
+        public Task<bool> Login(LoginViewModel loginVM);
+        public Task Logout();
+
+        public List<IdentityRole> GetRoles();
+        public Task<string> CreateRole(string Name);
+        public IdentityRole GetRoleById(string roleId);
+
+    }
+}
